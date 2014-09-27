24ae29de4f83c2d73ef0cedfc842cb40968b5545
Landon1013 <landon@thecallfamily.com>
2014-09-27 12:39:05 -0600

    Updates styles.css and animation.js

--- js/animation.js
+++ js/animation.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
+
     $("#tweet-controls").hide();
+    $(".stats").hide();
+    $(".reply").hide();
+
+    $("#stream").on("click", ".tweet", function(){
+        $(this).find(".stats").show();
+        $(this).find(".reply").show();
+    });
 
     $(".tweet-compose").on("click", function() {
         $(".tweet-compose").css("height", "5em");
@@ -26,6 +34,20 @@ $(document).ready(function(){
             $(".button").removeAttr("disabled", "disabled");
         }
 
+    });
+
+        $(".button").on("click", function(e) {
+
+            var el = $(".tweet:first").clone();
+
+            el.find(".avatar").prop("src", "img/alagoon.jpg");
+            el.find(".fullname").html("Cahlan Sharp");
+            el.find(".username").html("@cahlan");
+            el.find(".tweet-text").html($(".tweet-compose").val());
+            el.find(".tweet-text").html();
+            $("#stream").prepend(el);
+
+        //$("#stream").prepend(".tweet-compose.val()");
     });
 
 
